af066f1b01cea1625ab54a5ca0c02211739ee6e2
Vladimir Khodyrev <khodyrev.v.a@gmail.com>
2021-07-20 16:53:54 +0300

    Add product table

---
+++ common/components/Products/ProductsTable/ProductsTable.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import Collapse from 'antd/lib/collapse/Collapse';
+import ProductsTableGroupModel from '../../../../models/Products/ProductsTableGroupModel';
+import ProductItem from '../ProductItem/ProductItem';
+import styles from './ProductsTable.module.css'
+import {prepareTableData} from '../../../../utils/utils';
+import ProductItemModel from '../../../../models/Products/ProductItemModel';
+const { Panel } = Collapse;
+
+type Props = {
+    onSelectItem: (item: ProductItemModel) => void
+}
+
+const ProductsTable: React.FC<Props> = (props: Props) => {
+    const { onSelectItem } = props;
+    const [tableData, setTableData] = useState<ProductsTableGroupModel[]>([]);
+
+    useEffect(()=>{
+        if(tableData?.length < 1){
+            getData();
+        }
+        setInterval(getData, 15000)
+    }, [])
+
+    const getData = async () => {
+        const products = await fetch('https://raw.githubusercontent.com/nakukop/test/main/products.json')
+            .then(res => res.json())
+        const names = await fetch('https://raw.githubusercontent.com/nakukop/test/main/names.json')
+            .then(res => res.json())
+        setTableData(prepareTableData(products.Value.Goods, names))
+    }
+
+    return (
+        <>
+            {tableData.map((table,i) =>
+                <Collapse
+                    defaultActiveKey={['1']}
+                    expandIconPosition='left'
+                    className={styles.tableProductsContainer}
+                    key={`collapsed-product-table-${i}`}
+                >
+                    <Panel className={styles.tableProductsPanel} header={table.groupName} key="1">
+                        {table.items?.map((product, i)=> <ProductItem onItemClick={onSelectItem} key={`product-${product.price}-${i}`} item={product}/>)}
+                    </Panel>
+                </Collapse>
+            )}
+    </>)
+}
+
+
+export default ProductsTable;
